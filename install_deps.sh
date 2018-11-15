c33931ede9aa3abc972283cd0045c1a18c4cae0e
roentgen <fukasawa666@gmail.com>
2018-11-15 21:17:35 +0900

    heading for the frugalos/* repository

--- install_deps.sh
+++ install_deps.sh
@@ -25,9 +25,9 @@ cd ../
 #
 # jerasure
 #
-git clone https://github.com/roentgen/jerasure.git
+git clone https://github.com/frugalos/jerasure.git
 cd jerasure/
-git checkout 87f052e 
+git checkout c9851639f4830e516e48797832ec30fe82a43e28
 autoreconf --force --install
 CFLAGS="-I${BUILD_DIR}/include" LDFLAGS="-L${BUILD_DIR}/lib" ./configure --disable-shared --enable-static --with-pic --prefix $BUILD_DIR
 make $MAKE_FLAGS install
@@ -36,13 +36,13 @@ cd ../
 #
 # liberasurecode
 #
-git clone https://github.com/roentgen/liberasurecode.git
-cd liberasurecode/
-git checkout 8331347
+git clone https://github.com/frugalos/openstack_liberasurecode.git
+cd openstack_liberasurecode/
+git checkout 240333686eeae71ea1b94669242a8a6f4f51e260
 ./autogen.sh
 CFLAGS="-I${BUILD_DIR}/jerasure/include -I${BUILD_DIR}/include"
 if [ "$(uname)" == "Darwin" ]; then
     CFLAGS="$CFLAGS -Wno-error=address-of-packed-member"
 fi
-CFLAGS=$CFLAGS LIBS="-lJerasure" LDFLAGS="-L${BUILD_DIR}/lib" ./configure --disable-shared --with-pic --prefix $BUILD_DIR
+CFLAGS=$CFLAGS LDFLAGS="-L${BUILD_DIR}/lib" ./configure --disable-shared --with-pic --prefix $BUILD_DIR
 make $MAKE_FLAGS install
